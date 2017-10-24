84a6160f43493ebf23e510c30a04f71e1774698f
Vinit Mahajan <vinitmahajan1@gmail.com>
2017-10-24 11:43:13 +0530

    after diwali 1

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>welcome tpo portal</title>
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Flat Buttons Submit Forms,Button hover effects,Tool tips, ancher link hover effects,Elements"./>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div class="buttons">
+            <div class="span2">
+                <a href="questions/question.php">
+    		  <i>Ask a Question</i>
+    	       </a>
+            </div>
+    </div>
+ 	<div class="submit-forms">
+ 		  <form class="blue">
+			<input type="text" size="30px"  value="Enter your answer" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your answer';}">
+		            <input type="submit" value="Submit">
+		 </form>
+	</div>
+    
+</body>
+</html>
---
+++ questions/question.php
@@ -0,0 +1,59 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Techno Contact Form Flat Responsive Widget Template :: w3layouts</title>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- for-mobile-apps -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="Techno Contact Form Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!-- //for-mobile-apps -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!--Google Fonts-->
+<link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+</head>
+<body>
+<div class="wthree-dot">
+	<h1>Placement Portal</h1>
+	<div class="profile">
+		<div class="wrap">
+			<!-- contact -->
+			<div class="contact">
+				<div class="contact-row agileits-w3layouts">  
+					<div class="contact-w3lsleft">
+						<div class="contact-grid agileits">
+							<h4> Ask a Question? </h4>
+							<form action="qask.php" method="post"> 
+								<input type="text" name="Name" placeholder="Enter your PRN Number" required="">
+								<textarea name="Message" placeholder="Enter your question" required=""></textarea>
+								<input type="submit" value="Submit" >
+							</form> 
+						</div>
+					</div>
+					<div class="contact-w3lsright">
+						<div class="agileits-contact-right">
+							<h2>Request For Answer</h2>
+							
+						</div>
+					</div>
+					<div class="clear"> </div>
+				</div>	
+			</div>
+			<!-- //contact --> 
+			<div class="wthree_footer_copy">
+				<p>© 2017 Techno Contact Form. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
